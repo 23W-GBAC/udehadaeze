273494e3c615c1ee5d26faef6568ed393c01da38
udehadaeze <148863379+udehadaeze@users.noreply.github.com>
2023-12-23 03:55:09 +0100

    Update MyJavaFile.java

--- MyJavaFile.java
+++ MyJavaFile.java
@@ -1,33 +1,42 @@
 import java.util.Scanner;
 
-public class Genotype {
+  public class individualData {
     public static void main(String[] args) {
+        System.out.println("WELCOME to Ada's BMICalculator!!!");
+        System.out.println("Notice: This Calculator is intended for use by individuals over the age of 18");
+        
         Scanner keyboard = new Scanner(System.in);
-
-        System.out.print("Please enter the genotype for Father: ");
-        String fatherGenotype = keyboard.next();
-
-        System.out.print("Please enter the genotype for Mother: ");
-        String motherGenotype = keyboard.next();
-
-        genotypeCalculator(fatherGenotype, motherGenotype);
+        
+        System.out.print("Enter your Gender: ");
+        String gender = keyboard.next();
+        
+         System.out.print("Enter your Weight in kg: ");
+        double weight = keyboard.nextDouble();
+        
+         System.out.print("Enter your Height in cm: ");
+        double height = keyboard.nextDouble();
+        
+        double bmiValue = bmiCalculatorFormula(weight,height);
+        bmiDisplayresult(bmiValue);
     }
-
-    public static void genotypeCalculator(String fatherGenotype, String motherGenotype) {
-        if ((fatherGenotype.equals("AA") && motherGenotype.equals("AS")) || (fatherGenotype.equals("AS") && motherGenotype.equals("AA"))) {
-            System.out.println("Parents have a 50:50 chance of having a child with AA genotype or AS genotype");
-        } else if ((fatherGenotype.equals("AA") && motherGenotype.equals("SS")) || (fatherGenotype.equals("SS") && motherGenotype.equals("AA"))) {
-            System.out.println("Parents have a 100% chance of having a child with AS genotype");
-        } else if ((fatherGenotype.equals("AS") && motherGenotype.equals("SS")) || (fatherGenotype.equals("SS") && motherGenotype.equals("AS"))) {
-            System.out.println("Parents have a 50:50 chance of having a child with AS genotype or SS genotype");
-        } else if (fatherGenotype.equals("AA") && motherGenotype.equals("AA")) {
-            System.out.println("Parents have a 100% chance of having a child with AA genotype");
-        } else if (fatherGenotype.equals("AS") && motherGenotype.equals("AS")) {
-            System.out.println("Parents have a 100% chance of having a child with AS genotype");
-        } else if (fatherGenotype.equals("SS") && motherGenotype.equals("SS")) {
-            System.out.println("Parents have a 100% chance of having a child with SS genotype");
-        } else {
-            System.out.println("Invalid Input entered");
-        }
-    }
+    public static double bmiCalculatorFormula(double weight, double height) {
+    double heightInMeters = height / 100;
+    double bmiValue = weight / (heightInMeters * heightInMeters);
+    return bmiValue;
 }
+  public static void bmiDisplayresult(double bmiValue){
+      if(bmiValue < 18.5 ){
+           System.out.println("Underweight");
+      } else if (bmiValue >=18.5 && bmiValue <= 24.9){
+         System.out.println("Normal weight"); 
+      } else if (bmiValue >=25.0 && bmiValue <=29.9){
+          System.out.println("Overweight"); 
+      } else {
+           System.out.println("obesity");
+      }
+      
+  }
+    
+}
+
+
