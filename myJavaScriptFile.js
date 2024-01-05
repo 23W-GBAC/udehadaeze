fdcb2312b8b99be25a95d8dc7036be5aff089b5a
udehadaeze <148863379+udehadaeze@users.noreply.github.com>
2024-01-05 13:54:18 +0100

    Update myJavaScriptFile.js

--- myJavaScriptFile.js
+++ myJavaScriptFile.js
@@ -1,1 +1,43 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function bmiCalculatorFormula(weight, height) {
+  const heightInMeters = height / 100;
+  const bmiValue = weight / (heightInMeters * heightInMeters);
+  return bmiValue;
+}
+
+function bmiDisplayResult(bmiValue) {
+  if (bmiValue < 18.5) {
+    console.log("You are Underweight");
+  } else if (bmiValue >= 18.5 && bmiValue <= 24.9) {
+    console.log("You are Normal weight");
+  } else if (bmiValue >= 25.0 && bmiValue <= 29.9) {
+    console.log("You are Overweight");
+  } else {
+    console.log("You are obese");
+  }
+}
+
+console.log("WELCOME to Ada's BMICalculator!!!");
+console.log("Notice: This Calculator is intended for use by individuals over the age of 18");
+
+rl.question("Enter your Gender: ", (gender) => {
+  rl.question("Enter your Weight in kg: ", (weightInput) => {
+    const weight = parseFloat(weightInput);
+
+    rl.question("Enter your Height in cm: ", (heightInput) => {
+      const height = parseFloat(heightInput);
+
+      const bmiValue = bmiCalculatorFormula(weight, height);
+      bmiDisplayResult(bmiValue);
+
+      rl.close();
+    });
+  });
+});
 
